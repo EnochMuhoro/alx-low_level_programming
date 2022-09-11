0fb7fac8e590ea602f14b408a6004acccf49ecc6
Enoch Muhoro <enoch15muhoro@gmail.com>
2022-09-11 11:57:57 -0700

    Compiling a c file

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,6 +2,8 @@
 
 /* Printing "Programming is like building a multilingual puzzle" */
 
-int main(void){
-	puts("Programming is like building a multilingual puzzle");
-	return(0);}
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return(0);
+}
