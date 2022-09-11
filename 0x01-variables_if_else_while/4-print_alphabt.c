1bdacf1fc3dd5ea45e380ca6e7b3a3bcfbe0049e
Enoch Muhoro <enoch15muhoro@gmail.com>
2022-09-11 14:26:01 -0700

    lowercase except e and q

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - print alphabet in lowercase except e and q
+ * Return: 0 if successfull
+ */
+int main(void)
+{
+	char ch = 'a';
+
+	while (ch <= 'z')
+	{
+		if (ch != 'e' && ch != 'q')
+		{
+			putchar(ch);
+		}
+		ch++;
+	}
+	putchar('\n');
+	return (0);
+}
