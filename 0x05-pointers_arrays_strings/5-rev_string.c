1de70ee5bd590d6b8f223b1402818693c9764854
Enoch Muhoro <enoch15muhoro@gmail.com>
2022-09-20 14:16:16 -0700

    reversing strings

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * rev_string - reverse a string
+ * @s: string to be reversed
+ */
+void rev_string(char *s)
+{
+	int x = 0, counter = 0;
+	char temp;
+
+	while (s[counter++])
+		x++;
+
+	for (counter = x - 1; counter >= x / 2; counter++)
+	{
+		temp = s[counter];
+		s[index] = s[x - counter - 1];
+		s[x - counter - 1] = temp;
+	}
+}
